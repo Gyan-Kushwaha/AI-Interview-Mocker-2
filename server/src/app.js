49e6aa1144248f2bb768ffd238f3de9012c45577
Gyan Kushawaha <gyankushwaha77@gmail.com>
2025-08-17 14:20:07 +0530

    change

--- server/src/app.js
+++ server/src/app.js
@@ -1,33 +1,32 @@
 const express = require("express");
 const cors = require("cors");
-const dotenv = require("dotenv");
 const cookieParser = require("cookie-parser");
-const connectDB = require("./database/database");
-
-// Import Routes
-const userRoutes = require("./routes/user.routes");
-const mockinterviewRoutes = require("./routes/mockinterview.routes");
-const geminiRoutes =require("./routes/gemini.routes");
-const healthRoutes = require("./routes/health.routes");
+const dotenv = require("dotenv");
 
 dotenv.config();
 
+
+const userRoutes = require("./routes/user.routes");
+const mockinterviewRoutes = require("./routes/mockinterview.routes");
+const geminiRoutes = require("./routes/gemini.routes");
+const healthRoutes = require("./routes/health.routes");
+
 const app = express();
 
-// Database Connection
-connectDB();
 
+app.use(cors({
+  origin: process.env.FRONTEND_URL,
+  credentials: true,
+}));
 
 app.use(express.json());
 app.use(express.static("public"));
 app.use(cookieParser());
 
-// API Routes
-// API Routes
 app.use("/api/v1/health", healthRoutes);
 app.use("/api/v1/users", userRoutes);
 app.use("/api/v1/mockinterview", mockinterviewRoutes);
 app.use("/api/v1/ai", geminiRoutes);
-
+// -------------------------
 
 module.exports = app;
--- server/src/server.js
+++ server/src/server.js
@@ -1,18 +1,11 @@
 const app = require("./app");
-const dotenv = require("dotenv");
-const cors = require("cors");
-
-
-dotenv.config();
-console.log("The FRONTEND_URL environment variable is: ", process.env.FRONTEND_URL);
-
-app.use(cors({
-  origin: process.env.FRONTEND_URL, 
-  credentials: true,
-}));
+const connectDB = require("./database/database");
 
 const PORT = process.env.PORT || 5000;
 
+
+connectDB();
+
 app.listen(PORT, () => {
-  console.log(`🚀 Server is running on http://localhost:${PORT}`);
+  console.log(`🚀 Server is listening on port ${PORT}`);
 });
